908d281ea78559f493fa86435760557e993afcf1
swifferwarrior <kdhailey21@gmail.com>
2019-12-01 15:28:17 -0600

    Complete repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let str = string;
+    let strRepeat = '';
+    let i = 0;
 
+    if (number < 0){
+        return('ERROR');
+    }
+    
+    while (i < number){
+        strRepeat += str;
+        i++;
+    }
+    
+  return(strRepeat);  
 }
 
 module.exports = repeatString
