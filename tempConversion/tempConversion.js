054e5a71e642122c467d0e2efad22b71ef55bf65
swifferwarrior <kdhailey21@gmail.com>
2019-12-07 23:15:27 -0600

    Complete tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  c = Math.round(celsius * 10) / 10;
+  return c;
 }
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let fahrenheit = celsius * (9 / 5) + 32;
+  f = Math.round(fahrenheit * 10) / 10;
+  return f;
 }
 
 module.exports = {
