ddef2d886bfd2b8b8873fa050e8d872590d692a8
swifferwarrior <kdhailey21@gmail.com>
2020-06-11 22:45:23 -0500

    Started palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function() {
+const palindromes = (string) => {
+    string = string.replace(/\s+/g, "");
+    revString = string;
+    revStringArray = revString.split(); //splits string into array of substrings
+    revStringArray.reverse();
+    revString = revStringArray.join();
 
-}
+    string === revString ? true : false;
+};
+
+// function removeSpace(string){
+//     string = string.trim();
+//     return string
+// }
+
+// function reverseString(){
+
+// }
+
+
 
 module.exports = palindromes
