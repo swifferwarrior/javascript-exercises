eb5a64a74fcd0a2e47d14518f252ce6ac37b6173
swifferwarrior <kdhailey21@gmail.com>
2020-06-16 21:37:25 -0500

    First draft for findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let now = new Date().getFullYear();
+    let age = () => {
+        for (let person of people){ 
+            if (person.yearOfDeath == undefined){
+                person.age = now - person.yearOfBirth; //Age if alive
+            }
+         
+            person.age = person.yearOfDeath - person.yearOfBirth; //Age if dead
+    }
 
-}
+
+    }
+
+    console.table("table 1" + people.age);
+
+    people.sort((a,b) => a.age > b.age ? 1 : -1);
+
+    console.table(people);
+    console.log(people[0].name)
+};
 
 module.exports = findTheOldest
